92f1397438152e27102f5b6bda69b8982cf6eb66
jairoMolina9 <33586685+jairoMolina9@users.noreply.github.com>
2019-02-02 18:07:33 -0500

    Update array_basedList.cpp

--- arrayBasedList/array_basedList.cpp
+++ arrayBasedList/array_basedList.cpp
@@ -5,26 +5,95 @@
 */
 
 #include <time.h>
-#include "list.h"
+#include "list.cpp"
+
+void startMenu(ulist <int> &);
+void printList(ulist <int> &);
+void insertItem(ulist <int> &);
+void deleteItem(ulist <int> &);
+void searchItem(ulist <int> &);
 
 int main()
 {
 
-  srand(time(NULL));
+  int max;
 
-  arrayList arr1;
+  cout << "Enter max amount" << endl;
+  cin >> max;
 
-  for(int i = 0 ; i < 5 ; i++)
-  {
-    arr1.insertItem( rand() % 100 + 1 );
-  }
+  ulist <int> l1(max);
 
-  arr1.show();
+  startMenu(l1);
 
-  arr1.deleteItem(4);
+}
 
-  cout << "\nupdated \n\n";
+void startMenu(ulist <int> &l2)
+{
+   int option = 0;
 
-  arr1.show();
+   while(option != -1)
+   {
+         cout << "\nEnter option \n1. Insert \n2. Delete \n3. Search \n4. Print\n\nTO END TYPE -1\n";
+         cin >> option;
 
+      switch(option)
+      {
+
+         case 1:
+                insertItem(l2);
+                break;
+         case 2:
+                deleteItem(l2);
+                break;
+
+         case 3:
+                searchItem(l2);
+                break;
+
+         case 4:
+                printList(l2);
+                break;
+         default:
+                break;
+
+      }
+   }
+}
+
+void insertItem(ulist <int> &l2)
+{
+
+   int item;
+   cout << "\nInsert number : " << endl;
+   cin >> item;
+
+   l2.insertItem(item);
+
+}
+
+void deleteItem(ulist <int> &l2)
+{
+   int item;
+   cout << "\nInsert number : " << endl;
+   cin >> item;
+
+   l2.deleteItem(item);
+}
+
+void searchItem(ulist <int> &l2)
+{
+   int item;
+
+   cout << "\nInsert number: " << endl;
+   cin >> item;
+
+   bool found = l2.searchItem(item);
+
+   (found) ? cout << "\n" << item << " exists on the list\n" : cout << "\n" << item << " does not exist\n";
+}
+
+void printList(ulist <int> &l2)
+{
+   cout << endl;
+   l2.printList();
 }
