566cafd8ae32cb9ab7545f132603c451002e9bd4
jairoMolina9 <33586685+jairoMolina9@users.noreply.github.com>
2019-02-04 00:41:32 -0500

    Update list.cpp

--- arrayBasedList/list.cpp
+++ arrayBasedList/list.cpp
@@ -9,10 +9,44 @@ ulist<x> :: ulist(int max)
 }
 
 template <class x>
+ulist<x> ::ulist(const ulist<x>& other)
+{
+    max = other.max;
+    length = other.length;
+    info = new x[max]; //dynamic array
+    
+    for(int i = 0; i < max; i++)
+    {
+        info[i] = other.info[i];
+    } 
+} //end copy constructor
+
+template <class x>
 ulist<x> :: ~ulist()
 {
    delete [] info;
+   info = nullptr;
 }
+
+template <class x>
+ulist<x>& ulist<x>:: operator=( ulist<x> & list_2)
+{
+    if (this != &list_2)
+    {
+        delete []info;
+        info = nullptr;
+        
+        this->length = list_2.length;
+        this->max  = list_2.max;
+        info = new x[max];
+        
+        for(int i = 0; i < max; i++)
+        {
+            this->info[i] = list_2.info[i];
+        }    
+    }
+    return *this;
+} //end of overloaded = function
 
 template <class x>
 void ulist<x> :: makeEmpty()
@@ -43,11 +77,28 @@ void ulist<x> :: insertItem (x item)
 {
    if(!isFull())
    {
-      info[length] = item;
-      length++;
+       if(isEmpty())
+       {
+           info[length] = item;
+       }
+       else{
+           //sorting ascending order
+           for(int i = 0; i < length; i++)
+           {
+               x dummy;
+               if(item > info[i])
+               {
+                   dummy = info[i];
+                   info[i] = item;
+                   info[length] = dummy;
+               }
+           }
+       }
+       
+       length++;
    }
    else
-   cout << "\n\n LIST IS FULL \n\n";
+       cout << "\n\n LIST IS FULL \n\n";
 }
 
 template <class x>
