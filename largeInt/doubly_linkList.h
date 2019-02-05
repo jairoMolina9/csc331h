91e90d9296048e9607cfb6d79fc37acf36df9bce
jairoMolina9 <jairo.molina@stu.bmcc.cuny.edu>
2019-02-05 17:41:33 -0500

    Update

---
+++ largeInt/doubly_linkList.h
@@ -0,0 +1,20 @@
+
+template <class dataType>
+class doublyList
+{
+private:
+   dataType *back;
+   dataType *next;
+
+public:
+   doublyList();
+   doublyList(dataType value);
+   ~doublyList();
+   doublyList node(dataType value);
+
+};
+   template <class dataType>
+   doublyList<dataType> node(dataType value)
+   {
+
+   }
--- largeInt/main.cpp
+++ largeInt/main.cpp
@@ -1,2 +1,11 @@
 //Jairo Molina
-//
+//part1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+}
