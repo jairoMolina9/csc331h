542d6f0285589add0d02b979ac2f73a0ecc285f2
jairoMolina9 <33586685+jairoMolina9@users.noreply.github.com>
2019-03-02 22:43:49 -0500

    Update linkedList.h

--- linkedList/linkedList.h
+++ linkedList/linkedList.h
@@ -1,3 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   linkedList.h
+ * Author: molin
+ *
+ * Created on March 2, 2019, 9:48 PM
+ */
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
 #include <iostream>
 using namespace std;
 
@@ -25,7 +41,7 @@ public:
   void copyAll(const List &);
   int get_length();
   bool isEmpty();
-  void search();
+  bool search(type);
   void printFromFront();
   void printFromBack();
   void printEdges();
@@ -33,3 +49,5 @@ public:
   // iterator function from first to last
   // iterator function from last to first
 };
+
+#endif /* LINKEDLIST_H */
