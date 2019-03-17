c3d8e6efbb1a95b3cbd5702a1401db6000ba7d9c
jairoMolina9 <33586685+jairoMolina9@users.noreply.github.com>
2019-03-17 02:41:09 -0400

    Update main.cpp

--- stack/main.cpp
+++ stack/main.cpp
@@ -3,25 +3,64 @@
 */
 
 #include "stack.cpp"
+
+int preference(char oprt)
+{
+    int preference = 0;
+    switch (oprt)
+    {
+        case '+':
+        case '-':
+            preference = 1;
+            break;
+        case '*':
+        case '/':
+            preference = 2;
+        default:
+            break;
+    }
+    
+    return preference;
+}
+
 int main()
 {
-
-   string eqt = "3+2*1";
-
-   char eqt_1 [eqt.length()];
-
-   for(int i = 0 ; i <= eqt.length() ; i++)
-   {
-      eqt_1[i] = eqt[i];
-   }
-
-   int arg1 = 0;
-   arg1 = eqt_1[0] - '0';
-   int arg2 = 0;
-   arg2 = eqt_1[2] - '0';
-   int arg3 = 0;
-   arg3 = eqt_1[4] - '0';
-
-   cout << arg1 + arg2 * arg3 << endl;
-
+    Stack<int> operands;
+    Stack<char> operators;
+    
+    string exp = "";
+    
+    cout << "Type INFIX expression" << endl;
+    getline(cin, exp);
+    
+    for(int i = 0 ; i < exp.length() ; i++)
+    {
+        int value = 0;
+        
+        if(exp[i] == ' ')
+        {
+            continue;
+        }
+        else if (exp[i] == '(')
+        {
+            operators.push(exp[i]);
+        }
+        else if(isdigit(exp[i]))
+        {
+            while(i < exp.length() && isdigit(exp[i]))
+            {
+                value = (value*10) + exp[i] - '0';
+            }
+            operands.push(value);
+        } else if (exp[i] == ')'){
+            while(!operators.isEmptyStack() && operators._top() != '(')
+            {
+                //
+            }
+            
+        }
+        else {
+            operators.push(exp[i]);
+        }
+    }
 }
