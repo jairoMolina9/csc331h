6ab31c8e3a8fe3294881c3c243cbaa8542bf61f0
jairoMolina9 <molinaandres9991@gmail.com>
2019-05-06 13:29:02 -0400

    add

--- largeInt/largeInt.h
+++ largeInt/largeInt.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   largeInt.h
  * Author: molin
  *
@@ -19,97 +19,90 @@
 template <class type> class largeInt {
 private:
   string numbers;
-  bool neg;
   List<type> list;
   typename List<type>::Iterator iter;
 public:
   largeInt(){
       numbers = "";
-      neg = false;
       iter = nullptr;
   }
-  
+
   friend istream &operator>>( istream  &input, largeInt<type> &A ) {
         input >> A.numbers;
-        if(A.numbers[0] == '-'){
-            A.numbers.erase(0,1);
-            A.neg = true;
-        }
         A.setNodes();
         return input;
   }
-  
+
   friend ostream &operator<<( ostream  &output, const largeInt &A ) {
         output << "Numbers: " << A.numbers;
         return output;
      }
-  
+
  void setNodes() {
      for(int i = 0; i < numbers.length(); i++){
          list.insert_back(numbers[i] - '0');
      }
      iter = list.end(); //automatically sets iterator ready to calculate
   }
- 
+
  void printValues() {
      list.printFromFront();
  }
- 
- void is_negative(){
-     if(neg){
-         cout << "negative" << endl;
-     } else {
-         cout << "positive" << endl;
-     }
- }
- 
+
+
  largeInt operator+( largeInt<type> &other) {
-     is_negative();
-     largeInt<int> result;
-     
-     int dummy = 0;
-     int carry = 0;
-     bool extra_1 = true;
-     bool extra_2 = true;
-     
-    while(iter != nullptr || other.iter != nullptr){
-        if(*iter != 9)
-            extra_1 = false;
-        
-        if(*(other.iter) != 9)
-            extra_2 = false;
-        
-        if(other.iter == nullptr){
-            dummy = (*iter + carry);
-            --iter;
-        }
-        else if(iter == nullptr) {
-            dummy = *(other.iter) + carry;
-            --(other.iter);
-        }
-        else{
-            dummy = (*(other.iter) + *iter) + carry;
-            --iter;
-            --(other.iter);
-        }
-        if(dummy > 9){
-            carry = 1;
-            result.list.insert_front(dummy-10);
-            
-        } else {
-            carry = 0;
-            result.list.insert_front(dummy);
-        }
-    }
-     
-     if(extra_1 || extra_2){
-         result.list.insert_front(1);
-     }
+   largeInt<int> result;
+
+   int dummy = 0;
+   int carry = 0;
+
+
+   while(iter != nullptr || other.iter != nullptr){
+
+      /*adding two digits
+        case 1: list1 is at the end
+        case 2: list2 is at the end
+        case 3: add value list1 & list2
+      */
+      if(other.iter == nullptr){
+         dummy = (*iter + carry);
+         --iter;
+      }
+      else if(iter == nullptr) {
+         dummy = *(other.iter) + carry;
+         --(other.iter);
+      }
+      else{
+         dummy = (*(other.iter) + *iter) + carry;
+         --iter;
+         --(other.iter);
+      }
+
+      /*addition results
+        case 1: result is double digit
+        case 2: result is single digit
+      */
+      if(dummy > 9){
+         carry = 1;//always 1, max is 9+9 = '1''8'
+         result.list.insert_front(dummy-10);//insert single digit
+      } else {
+         carry = 0;
+         result.list.insert_front(dummy);
+      }
+   }
+
+   /* Check if 9 is the first value in the lists*/
+   iter = list.begin();
+   other.iter = other.list.begin();
+
+   if( *iter == 9 || *(other.iter) == 9){
+      result.list.insert_front(1); //inserts 1 in front
+   }
+
   return result;
  }
-    
+
 };
 
 
 #endif /* LARGEINT_H */
-
